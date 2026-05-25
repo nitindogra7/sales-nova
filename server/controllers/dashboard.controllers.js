e6dd333927c4e9e81ab73c87a7fad8a156fb7d38
Nitin Dogra <dogranitin579@gmail.com>
2026-05-25 20:08:52 +0530

    get dashboard bug fixed

--- server/controllers/dashboard.controllers.js
+++ server/controllers/dashboard.controllers.js
@@ -1,8 +1,14 @@
 import User from '../models/user.model.js';
 export const getDashboard = async (req, res) => {
-  const user = req.user;
-  const userdata = await User.findById(user._id).select('-password -otpId');
-  res
-    .status(200)
-    .json({ message: `Welcome to the dashboard, ${userdata.username}!` });
+  try {
+    const user = req.user;
+    const userdata = await User.findById(req.user.id).select('-password');
+    console.log(userdata);
+    res
+      .status(200)
+      .json({ message: `Welcome to the dashboard, ${userdata.companyName}!` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
 };
